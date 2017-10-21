f54235e9cb0a8a718ece1d7fbdc85f4d062ad1f9
Michael Kero <mkero@thorntontomasetti.com>
2017-10-21 10:59:09 -0400

    Added VS projects

---
+++ Revilations.Revit/RevilationApplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+
+namespace Revilations.Revit {
+    public class RevilationApplication : IExternalApplication {
+        public Result OnShutdown(UIControlledApplication application) {
+            throw new NotImplementedException();
+        }
+
+        public Result OnStartup(UIControlledApplication application) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Revilations.Revit/RevilationCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Revilations.Revit
+{
+    public class RevilationCommand : IExternalCommand {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
+            throw new NotImplementedException();
+        }
+    }
+}
